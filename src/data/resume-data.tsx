aca694fa1f09c3173c0cf5159058657af7e25dd1
Ali Mostafa Radwan <66019196+AliMostafaRadwan@users.noreply.github.com>
2024-01-17 02:33:15 +0200

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,206 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
+  name: "Ali Mostafa Radwan",
   initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  location: "ismailila, Egypt",
+  locationLink: "https://www.google.com/maps/place/ismailia",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Machine Learning and Computer Vision Engineer focused on building products with extra attention to detail",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "With a passion for machine learning and computer vision, I am a highly motivated programmer with a track record of mastering new software packages and hardware technologies quickly. My exceptional background in these fields has equipped me with the skills and knowledge to excel in challenging projects.",
+  avatarUrl: "https://iili.io/JYrt33F.th.jpg",
+  personalWebsiteUrl: "https://aliradwan.netlify.app",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "alimosamin@gmail.com",
+    tel: "+201552652525",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/AliMostafaRadwan",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/ali-m-radwan/",
         icon: LinkedInIcon,
       },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
-      },
+      // {
+      //   name: "X",
+      //   url: "https://x.com/BartoszJarocki",
+      //   icon: XIcon,
+      // },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Egyptian Academy for Engineering and advanced Technology",
+      degree: "Bachelor's Degree in Electronics and Communications Engineering",
+      start: "2022 Oct",
+      end: "present",
     },
+    {
+      school: "Udacity Data analysis (online course)",
+      degree: "Data analysis Nanodegree",
+      start: "2022",
+      end: "2022 Mar",
+    }, 
+    {
+      school: "Udacity Machine Learning (online course)",
+      degree: "Machine Learning Nanodegree",
+      start: "2022 Jul",
+      end: "2022 Sep",
+    },
+    {
+      school: "Udacity (online course)",
+      degree: "Edge AI Fundamentals with OpenVINO™",
+      start: "2023",
+      end: "present",
+    },
+    {
+      school: "coursera (online course)",
+      degree: "Neural Networks and Deep Learning (DeepLearning.AI)",
+      start: "2023",
+      end: "present",
+    },
+    {
+      school: "Udemy (online course)",
+      degree: "Convolutional Neural Networks for Image Classification",
+      start: "2022 Nov",
+      end: "2022 Dec",
+    },
+    {
+      school: "Udemy (online course)",
+      degree: "Game Hacking With Artificial Intelligence - FPS Aimbot",
+      start: "2021 Nov",
+      end: "2021 Dec",
+    },
+    {
+      school: "Kaggle (online course)",
+      degree: "Kaggle Intro to Deep Learning",
+      start: "2022 Jan",
+      end: "2022 Jan",
+    },
+    {
+      school: "My Great Learning (online course)",
+      degree: "Computer Vision Essentials",
+      start: "2021 Apr",
+      end: "2021 Apr",
+    },
+
+    
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "Freelancer",
+      link: "https://freelancer.com",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      title: "python developer",
       logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      start: "2020",
+      end: "current",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "working on all sorts of projects related to machine learning or computer vision",
     },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
-    },
+    // {
+    //   company: "Upwork",
+    //   link: "https://clevertech.biz",
+    //   badges: ["Remote"],
+    //   title: "Lead Android Developer → Full Stack Developer",
+    //   logo: ClevertechLogo,
+    //   start: "2015",
+    //   end: "2021",
+    //   description:
+    //     "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+    // },
+    // {
+    //   company: "Jojo Mobile",
+    //   link: "https://bsgroup.eu/",
+    //   badges: [],
+    //   title: "Android Developer → Lead Android Developer",
+    //   logo: JojoMobileLogo,
+    //   start: "2012",
+    //   end: "2015",
+    //   description:
+    //     "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+    // },
+    // {
+    //   company: "Nokia Siemens Networks",
+    //   link: "https://www.nokia.com",
+    //   badges: [],
+    //   title: "C/C++ Developer",
+    //   logo: NSNLogo,
+    //   start: "2010",
+    //   end: "2012",
+    //   description: "Creating and testing software for LTE base stations",
+    // },
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Python",
+    "SQL",
+    "tensorflow",
+    "PyTorch",
+    "Seaborn",
+    "Panadas",
+    "Streamlit",
+    "Keras",
+    "scikit-learn",
+    "OpenCV",
+    "Transformars",
+    "PyQt5",
+    "PySide6",
+    "HTML/CSS",
+    "Tableau",
+    "Machine Learning",
+    "Computer Vision",
+    "NLP",
+    "Data Analysis",
+
+    
   ],
   projects: [
+  
     {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "GradeVision",
+      techStack: ["Python", "Machine Learning", "Computer Vision", "OpenCV","PyTorch","PyQt5"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
+        "An application designed to mark duplicated bubble sheets using machine learning and computer vision. The project involves a Python script designed to facilitate bubble sheet grading through the utilization of computer vision and machine learning techniques. Using Pyqt5 as a modern Interface with custom qss.",
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
+      title: "VARK TEST WITH AI RECOMMENDER",
+      techStack: ["Chat-GPT API", "Python", "Streamlit", "Machine Learning"],
       description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
+        "Using Chat-GPT API to get the perfect recommended learning style according to the VARK test measurements.",
 
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+    },
+    {
+      title: "Abstractivr Arabic Summarizer",
+      techStack: ["NLP", "Python", "BERT", "Machine Learning","Transformars"],
+      description: "NLP project for Arabic summarization using the SumArabic dataset.",
+    },
+    {
+      title: "SLAM, Simultaneous Localization and Mapping",
+      techStack: ["Computer Vision", "Python", "OpenCV", "Machine Learning"],
+      description: "Work in progress playing around with SLAM.",
+    },
+    {
+      title: "Remark-Office OMR automation using yolov5",
+      techStack: ["Automation", "Python", "yolov5", "Machine Learning", "Computer Vision", "OpenCV"],
+      description: "creating a third party app for Remark-Office OMR using yolov5 (outdated).",
+    },
+    {
+      title: "OCR with form classification using pytesseract",
+      techStack: ["Automation", "Python", "pytesseract", "Computer Vision", "OpenCV"],
+      description: "OCR with form classification using pytesseract.",
+    },
+    {
+      title: "Check More",
+      techStack: [""],
+      description: "Check my GitHub for more projects.",
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
+        label: "GitHub",
+        href: "https://www.github.com/AliMostafaRadwan",
+      }
     },
   ],
 } as const;
